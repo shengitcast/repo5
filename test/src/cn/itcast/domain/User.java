3a2819467fc54d6d030b805e664ba6039b9ae7cc
sheng <sheng@163.com>
2019-01-26 23:36:55 +0800

    把本地工程添加到仓库中

--- test/src/cn/itcast/domain/User.java
+++ test/src/cn/itcast/domain/User.java
@@ -6,4 +6,11 @@ public class User implements Serializable{
     private String name ;
     private int age ;
     private String address;
+
+    public static void main(String[] args) {
+        test();
+    }
+    public static void test(){
+        System.out.println("测试git");
+    }
 }
