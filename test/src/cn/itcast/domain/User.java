af943f506c74b9199b585599d557033ff3ed9af7
sheng <sheng@163.com>
2019-01-26 23:34:48 +0800

    把本地工程添加到仓库中

---
+++ test/src/cn/itcast/domain/User.java
@@ -0,0 +1,9 @@
+package cn.itcast.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+    private String name ;
+    private int age ;
+    private String address;
+}
